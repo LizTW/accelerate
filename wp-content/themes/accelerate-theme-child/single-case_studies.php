ce1b1314dd7bf6379bb7c5201596e9b662c11b07
Liz T <liz@liztracywriting.com>
2017-10-18 20:32:53 +0400

    wp get attachment added to single case study

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -14,12 +14,13 @@ get_header(); ?>
 	<div id="primary" class="site-content">
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
+
 				$services = get_field('services');
 				$client = get_field('client');
 				$link = get_field('site_link');
 				$image_1 = get_field('image_1');
 				$image_2 = get_field('image_2');
-				$size = "full";
+				$size = "full"; ?>
 
 				<article class="case-study">
 					<aside class = "case-study-sidebar">
@@ -29,17 +30,17 @@ get_header(); ?>
 
 				    <?php the_content(); ?>
 
-						<p><a href="<?php echo $link; ?>">Site Link</a></p>
+						<p><strong><a href="<?php echo $link; ?>">Site Link</a></strong></p>
 					</aside>
 
 					<div class="case-study-images">
-						<?php if($image_1) { ?>
+						<?php if($image_1) {
 							echo wp_get_attachment_image( $image_1, $size );
 					  <?php } ?>
-						<?php if($image_2) { ?>
+						<?php if($image_2) {
 							echo wp_get_attachment_image( $image_2, $size );
 						<?php } ?>
-						<?php if($image_3) { ?>
+						<?php if($image_3) { 
 							echo wp_get_attachment_image( $image_3, $size );
 						<?php } ?>
 
