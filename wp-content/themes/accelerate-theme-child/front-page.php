70971daef8b12596a78ba4b8ce83ec3f3232e11c
Liz T <liz@liztracywriting.com>
2017-10-19 17:24:36 +0400

    custom query added to front-page

--- wp-content/themes/accelerate-theme-child/front-page.php
+++ wp-content/themes/accelerate-theme-child/front-page.php
@@ -13,28 +13,52 @@
  */
 
 get_header(); ?>
+	<section class ="home-page">
+		<div class="site-content">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<div class='homepage-hero'>
+					<?php the_content(); ?>
+					<a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
+				</div>
+			<?php endwhile; // end of the loop. ?>
+		 </div><!-- .main-content -->
+	</section><!-- #primary -->
 
-<div id="primary" class="home-page hero-content">
-	<section class = "recent-posts">
+	<section class="featured-work">
+		<div class="site-content">
+			<h4>Featured Work</h4>
+
+			<ul class="homepage-featured-work">
+			<?php query_posts('posts_per_page=3'); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					$image_1 = get_field("image_1");
+					$size = "medium";
+				?>
+				<li class-"individual-featured-work">
+					<figure>
+						?php echo wp_get_attachment_image($image_1, $size);
+					</figure>
+
+					<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+				</li>
+				<?php endwhile; ?>
+			<?php wp_reset_query(); ?>
+		</ul>
+		</div>
+	</section><!--.home-age-->
+
+  <section class ="recent-posts">
 		<div class="site-content">
 			<div class="blog-post">
 				<h4>From the Blog</h4>
 					<?php query_posts('posts_per_page=1'); ?>
 						<?php while ( have_posts() ) : the_post(); ?>
-							<h3><?php the_title(); ?></h3>
+							<h2><?php the_title(); ?></h2>
 							<?php the_excerpt(); ?>
 						<?php endwhile; ?>
 					<?php wp_reset_query(); ?>
 			</div>
 		</div>
-	</section>
-	
-		<div class="main-content" role="main">
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
-				<a class="button" href="<?php echo site_url('/blog/') ?>">View Our Work</a>
-			<?php endwhile; // end of the loop. ?>
-		</div><!-- .main-content -->
-	</div><!-- #primary -->
+	</section><!--.home-age-->
 
 <?php get_footer(); ?>
