e270c471d479eaaa27ef1027659882af34a88ceb
Liz T <liz@liztracywriting.com>
2017-11-29 23:32:39 +0400

    404 page updated

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -41,10 +41,9 @@
  add_action( 'init', 'create_custom_post_types' );
 
  // Enqueue scripts and styles
- 
+
  function accelerate_child_scripts() {
-   wp_enqueue_style('accelerate-style', get_stylesheet_url() );
-   wp_enqueue_style('accelerate-google-fonts','//fonts.googleapis.com/css?family=Gloria+Hallelujah');
+   wp_enqueue_style('accelerate-child-google-fonts','//fonts.googleapis.com/css?family=Gloria+Hallelujah');
  }
 
-add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' )
+add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
