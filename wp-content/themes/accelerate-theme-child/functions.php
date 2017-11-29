fe701481a0127806b3ec7059461186d875b6ca93
Liz T <liz@liztracywriting.com>
2017-11-29 23:58:00 +0400

    twitter widget added

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -47,3 +47,20 @@
  }
 
 add_action( 'wp_enqueue_scripts', 'accelerate_child_scripts' );
+
+// Twitter Feed Dynamic sidebar
+
+function accelerate_theme_child_widget_init() {
+
+  register_sidebar( array(
+    'name' =>__( 'Homepage sidebar', 'accerelate-theme-child' ),
+    'id' => 'sidebar-2',
+    'description' => __( 'Appears on the static front page template', 'accelerate-theme-child' ),
+    'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+    'after_widget' => '</aside',
+    'before_title' => '<h3 class="widget-title">',
+    'after_title' => '</h3>',
+  ));
+}
+
+add_action( 'widgets_init', 'accelerate_theme_child_widget_init' );
